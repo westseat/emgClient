b3ab14e9adfc273b5f4eaedfea0545cb6a5aaa1a
kai.zhou <zhoukaisspu@163.com>
2019-08-13 23:37:16 +0800

    Add canvasManager and websocket reconnect

--- ws.js
+++ ws.js
@@ -40,11 +40,88 @@ var emgCanvas = {
         }
         this._context.stroke();
     },
-}
+};
 
+var canvasManager = {
+    _canvasContainer: {},
+    registerEmgCanvas: function(key, emgObj) {
+        this._canvasContainer[key] = emgObj;
+    },
+    updateCanvas: function() {
+        for(key in this._canvasContainer) {
+            this._canvasContainer[key].draw();
+        }
+    },
+    pushEmgData: function() {
+        for(key in this._canvasContainer) {
+           this._canvasContainer[key].pushValue(Math.floor(Math.random() * 300)); 
+        }
+    }
+};
+
+var webSocketManager = {
+        initWebSocket: function() {
+            console.log("[info]: start to connect socket");
+            if(globalContent.wsObj) {
+                delete globalContent.wsObj;
+            }
+            try {
+                var self = this;
+                this._socket = new WebSocket("ws://127.0.0.1:8876");
+                this._socket.onopen = function(e) {
+                    globalContent.wsObj = self;
+                };
+                this._socket.onmessage = function(event) {
+                    console.log(event);
+                };
+                this._socket.onclose = function(event) {
+                    console.log("[info]: socket is closed")
+                    setTimeout(() => {
+                        let wsobj = Object.create(webSocketManager);
+                        wsobj.initWebSocket();
+                    }, 1000);
+                };
+                this._socket.onerror = function(error) {
+                    console.log(error);
+                }
+            } catch (error) {
+                console.log("[error]: connect to sokcet error");
+                setTimeout(() => {
+                    let wsobj = Object.create(webSocketManager);
+                    wsobj.initWebSocket();
+                }, 1000);
+            }
+    }
+};
+
+var globalContent = {};
 window.addEventListener("load",function(){
-    var canvas = document.getElementById("ch1");
-    var ctx1 = canvas.getContext("2d");
+    let chArray = [];
+    let ctxArray = [];
+    let canvasObj = [];
+    for(let i = 1; i<=8; i++) {
+        let str = "ch" + i;
+        chArray.push(document.getElementById(str));
+    }
+    for(let i = 1; i<=8; i++) {
+        ctxArray.push(chArray[i-1].getContext("2d"));
+        let emg = Object.create(emgCanvas);
+        emg.initEmgCanvas(ctxArray[i-1]);
+        emg._setXspacing(10);
+        emg._setY(0,300);
+        //canvasObj.push(emg);
+        canvasManager.registerEmgCanvas("ch"+i, emg);
+    }
+//    var ch1 = document.getElementById("ch1");
+//    var ch2 = document.getElementById("ch2");
+//    var ch3 = document.getElementById("ch3");
+//    var ch4 = document.getElementById("ch4");
+//    var ch5 = document.getElementById("ch5");
+//    var ch6 = document.getElementById("ch6");
+//    var ch7 = document.getElementById("ch7");
+//    var ch8 = document.getElementById("ch8");
+//    var ctx1 = ch1.getContext("2d");
+//    var ctx2 = ch2.getContext("2d");
    // ctx1.moveTo(0, 0);
    // ctx1.lineTo(10,30);
    // ctx1.lineTo(20, 10);
@@ -57,23 +134,33 @@ window.addEventListener("load",function(){
    // setTimeout(() => {
    //     ctx1.clearRect(0, 0, 500, 300);
    // }, 2000);
-    var emg1 = Object.create(emgCanvas);
-    emg1.initEmgCanvas(ctx1);
-    emg1._setXspacing(10);
-    emg1._setY(0, 300);
-    var initValue = 2;
+//    var emg1 = Object.create(emgCanvas);
+//    emg1.initEmgCanvas(ctx1);
+//    emg1._setXspacing(10);
+//    emg1._setY(0, 300);
+//    var initValue = 2;
     setInterval(function(){
-        emg1.pushValue(Math.floor(Math.random() * 300));
-    },500);
+        //for(let i = 0; i< canvasObj.length; i++) {
+            //canvasObj[i].pushValue(Math.floor(Math.random() * 300));
+            canvasManager.pushEmgData();
+        //}
+        //emg1.pushValue(Math.floor(Math.random() * 300));
+    },200);
  //   setInterval(() => {
  //       emg1._context.clearRect(0, 0, 500, 300);
  //   }, 500);
     setInterval(() => {
-        console.log(emg1._container.length);
-        emg1._context.clearRect(0, 0, 500, 300);
-        console.log("draw canvas");
-        emg1.draw();
-    }, 500);
+        //console.log(emg1._container.length);
+        //emg1._context.clearRect(0, 0, 500, 300);
+        //console.log("draw canvas");
+        //emg1.draw();
+      //  for(let i =0; i< canvasObj.length; i++) {
+      //      canvasObj[i].draw();
+      //  }
+      canvasManager.updateCanvas();
+    }, 200);
+    let ws = Object.create(webSocketManager);
+    ws.initWebSocket();
    // let socket = new WebSocket("ws://127.0.0.1:8876");
    // socket.onopen = function(e) {
    //     console.log("socket is opne");
